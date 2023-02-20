76f8ef3e15cc70b8cd62b049ba48129bd47945f9
kaviyaponni <125038491+kaviyaponni@users.noreply.github.com>
2023-02-20 10:28:27 +0530

    uploading java program

---
+++ array.java
@@ -0,0 +1,25 @@
+package array;
+
+public class array {
+
+	public static void main(String[] args) {
+		int stu[];
+		stu=new int[5];
+		stu[0]=10;
+		stu[1]=11;
+		stu[2]=12;
+		stu[3]=13;
+		stu[4]=14;
+		System.out.println("Array elements are ");
+		for(int i=0;i<stu.length;i++) {
+		System.out.println(stu[i]);
+			
+		}
+
+
+
+
+
+	}
+
+}
